3204a14e61004496ba35884ac01c6f08afd2e327
abkrym5555 <momo542004@gmail.com>
2025-08-12 00:06:36 +0300

    add style and first look

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 😎</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list"> list</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>you have x items in your list , and you already packed x (x%)</em>
+    </footer>
+  );
 }
